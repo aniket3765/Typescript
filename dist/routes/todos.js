397ca6b8a63893647d21e8e9e7f619b91973e60c
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-10-01 00:00:47 +0530

    File system

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,24 +7,28 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     res.status(201).json({ message: "Todo added", newTod: newTodo, todos: todos });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex(todoItems => todoItems.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: "Updated todo", todos: todos });
     }
     res.status(404).json('Not able to find this Id');
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
     res.status(200).json({ message: "Todo deleted", todos: todos });
 });
 exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 import {Todo} from '../models/todo';
 
 let todos: Todo[] = [];
+type RequestBody = {text:string};
+type RequestParams = {todoId:string};
 
 const router = Router();
 
@@ -11,26 +13,30 @@ res.status(200).json({todos:todos})
 })
 
 router.post('/todo',(req, res, next)=>{
+   const body = req.body as RequestBody ;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text    }
+        text: body.text   }
 
         todos.push(newTodo);
         res.status(201).json({message:"Todo added", newTod:newTodo, todos:todos})
 })
 
 router.put('/todo/:todoId',(req, res, next)=>{
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex =  todos.findIndex(todoItems => todoItems.id === tid);
     if(todoIndex >=0 ){
-        todos[todoIndex] = { id: todos[todoIndex].id, text:req.body.text}
+        todos[todoIndex] = { id: todos[todoIndex].id, text:body.text}
         return res.status(200).json({message:"Updated todo", todos:todos})
     }
     res.status(404).json('Not able to find this Id')
 })
 
 router.delete('/todo/:todoId',(req, res, next)=> {
-todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+const params = req.params as RequestParams;
+todos = todos.filter(todoItem => todoItem.id !== params.todoId);
 res.status(200).json({message:"Todo deleted", todos:todos});
 })
 
